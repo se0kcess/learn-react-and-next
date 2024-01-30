34997d176bca20ff94ce0492bf545e0b3049160c
se0kcess <kimminsuk051@gmail.com>
2024-01-30 15:18:38 +0900

    Todo Create

--- todoList/src/components/TodoEditor.jsx
+++ todoList/src/components/TodoEditor.jsx
@@ -1,8 +1,38 @@
+import { useRef } from 'react';
+import { useState } from 'react'
 import "./TodoEditor.css"
 
-export default function TodoEditor (){
-  return <div className='TodoEditor'>
-    <input placeholder='새로운 Todo ...'/>
-    <button>추가</button>
-  </div>
+export default function TodoEditor({ onCreate }) {
+  const [content, setContent] = useState("");
+  const inputRef = useRef();
+  
+  const onChangeContent= (e) => {
+    setContent(e.target.value);
+  }
+
+  const onClick = () => {
+    if(content === ""){
+      inputRef.current.focus();
+      return;
+    }
+    onCreate(content)
+    setContent("");
+  }
+
+  const onKeyDown=(e)=>{
+    if(e.keyCode === 13){
+      onClick();
+    }
+  }
+
+  return (
+    <div className='TodoEditor'>
+      <input 
+        value = {content} 
+        onChange = {onChangeContent}  
+        onKeyDown = {onKeyDown}
+        placeholder='새로운 Todo ...'/>
+      <button onClick={onClick}>추가</button>
+    </div>
+  )
 }
