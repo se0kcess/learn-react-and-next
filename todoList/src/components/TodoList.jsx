d1a92e633f98a10cd80d577342dc25140fea298b
se0kcess <kimminsuk051@gmail.com>
2024-02-02 01:00:17 +0900

    useMemo

--- todoList/src/components/TodoList.jsx
+++ todoList/src/components/TodoList.jsx
@@ -1,6 +1,6 @@
 import TodoItem from './TodoItem';
 import "./TodoList.css";
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 export default function TodoList ({ todos, onUpdate, onDelete }) {
 
@@ -19,9 +19,40 @@ export default function TodoList ({ todos, onUpdate, onDelete }) {
       );
   }
 
+  const getAnalizedTodoData = () => {
+    console.log("투두 분석 함수 호충")
+    const totalCount = todos.length
+    const doneCount = todos.filter(todo => todo.isDone).length
+    const notDoneCount = totalCount - doneCount
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount
+    }
+  }
+
+  const {totalCount, doneCount, notDoneCount} = useMemo(()=>{
+    console.log("투두 분석 함수 호충")
+    const totalCount = todos.length
+    const doneCount = todos.filter(todo => todo.isDone).length
+    const notDoneCount = totalCount - doneCount
+
+      return {
+      totalCount,
+      doneCount,
+      notDoneCount
+    }
+  },[todos]);
+
   return (
   <div className='TodoList'>
     <h4>Todos</h4>
+    <div>
+      <div>전체 투두: {totalCount}</div>
+      <div>완료된 투두 : {doneCount}</div>
+      <div>미완료된 투두 : {notDoneCount}</div>
+    </div>
     <input
     value = {search}
     onChange = {onChangeSearch}
